2ef9bbf13ffb5cf4c6927f11d08bd6fd866fb26c
gracebunella <83432790+gracebunella@users.noreply.github.com>
2021-04-29 18:54:20 -0400

    Rotated Tree About a Point

--- sketch.js
+++ sketch.js
@@ -21,35 +21,40 @@ log = { today: [3],
  var axiom = "F";
  var rules = []
      rules[0] = { a: "F",
-                  //b: "FF+[+F-F-FL]-[-F+F+FL]"}
+                  //b: "FF+[++F-F-FL]-[--F+F+FL]"} //original
+                  b: "FF+[+FF-FL]-[-F+F-FL]"} //happy tree
+                  //b: "F+[+F-F+F]-[-FF+F]"}
                   //b: "F+[+F+F-FL]-[-FF+FL]"}
-                  b: "FF+[+FF-FL]-[-F+FL]"} //this one is nice
-                  //b: "FF+[+F-F+FL]-[-FFL]"}
 
-      // rules[1] = { a: "FL",
+      // rules[1] = { a: "L",
       //              //b: "FF+[+F-F-FL]-[-F+F+FL]"}
       //              //b: "FF+[+F+F-FL]--[F++FL]"}
-      //              b: "-[+F-F+F-F]"
+      //              b: "[FFL]"
       //            }
 
 
  var sentence = axiom;
- var len = 70;
- var strokeW = 6;
+ var len = 40;
+ var strokeW = 5;
 
  function setup(){
    //noCanvas();
    //createP(axiom);
    colorMode(HSB, 360, 100, 100, 100);
-   angle = PI/12;
+   ellipseMode(CENTER);
+   angle = PI/8;
    createCanvas(600, 600);
    background(100,0,100,100);
    turtle();
+
+   stroke(0, 0, 0);
+   strokeWeight(5);
+   //line(10, height/2, width/2, height/2);
  }
 
  function generate(){
-   len *= .7;
-   strokeW *= .7; //.7
+   len *= .65;
+   strokeW *= .6; //.7
    var nextSentence = "";
    for(var i = 0; i < sentence.length; i++){
      var current = sentence.charAt(i);
@@ -58,7 +63,7 @@ log = { today: [3],
        if(current == rules[j].a){
          found = true;
          nextSentence += rules[j].b;
-         break;
+         //break;
        }
      }
      if(!found){
@@ -70,15 +75,50 @@ log = { today: [3],
 
  }
 
+ function generate_2(){
+
+   len *= 1;
+   strokeW *= .6; //.7
+   for(var p = 1; p < log.week.length + 1; p++){
+     sentence += "-[+F-F+FL]";
+   }
+ }
+
  function mousePressed(){
 
    generate();
+   push();
+   translate(width/2,height/2);
    turtle();
+   pop();
 
+   push();
+   translate(width/2,height/2);
+   rotate(PI/3+random(-PI/12, PI/12));
+   turtle();
+   pop();
+
+   push();
+   translate(width/2,height/2);
+   rotate(PI*2/3+random(-PI/12, PI/12));
+   turtle();
+   pop();
+
+   push();
+   translate(width/2,height/2);
+   rotate(PI*3/3+random(-PI/12, PI/12));
+   turtle();
+   pop();
+
+   push();
+   translate(width/2,height/2);
+   rotate(PI*4/3+random(-PI/12, PI/12));
+   turtle();
+   pop();
  }
 
  function turtle(){
-   translate(width/2,height-20);
+   //translate(width/2,height/2);
    for (var i = 0; i < sentence.length; i++){
      var current = sentence.charAt(i);
      if (current == "F"){
@@ -87,15 +127,23 @@ log = { today: [3],
        line(0,0,0, -len);
        translate(0, -len);
 
+     } else if (current == "+"){
+       rotate(angle);
+     } else if (current == "-"){
+       rotate(-angle);
+     } else if (current == "["){
+       push();
+     } else if (current == "]"){
+       pop();
      } else if(current == "L"){
        if(strokeW < 4){
-         let leafGen = random(0, 7);
+         let leafGen = random(0, 5);
          let leaves = [];
 
          for(var n = 1; n < leafGen + 1; n++){
            if(n == 1){
              //leafDir = PI/2;
-             leafDir = 0;
+             leafDir = angle;
            } else if(n % 2 == 0){
              //leafDir = PI/3;
              leafDir = PI/4;
@@ -108,23 +156,15 @@ log = { today: [3],
            capacitanceNow = random(800, 12000);
            leaves[n] = new Leaf(capacitanceNow, hue, leafX, leafY, leafDir);
           }
-          rotate(random(PI/2, PI));
+          rotate(PI-angle*2);
           for(var m = 1; m < leaves.length; m++){
-           fill(map(m, 1, leaves.length, 75, 140), 70, map(m, 1, leaves.length, 100, 70), 70);
-           //translate(0, -leafY);
+           fill(map(m, 1, leaves.length, 8, 55), 70, map(m, 1, leaves.length, 100, 100), 70);
+           translate(0, -leafY);
            leaves[m].display();
           }
       }
-     } else if (current == "+"){
-       rotate(angle);
-     } else if (current == "-"){
-       rotate(-angle);
-     } else if (current == "["){
-       push();
-     } else if (current == "]"){
-       pop();
-     }
    }
+ }
  } //end turtle()
 
  function draw(){
@@ -144,19 +184,19 @@ log = { today: [3],
    display(){
 
      let leafLength = map(this.capNow, 800, 12000, 10, 20);
-     //rotate(this.leafDir);
+     rotate(this.leafDir);
 
      //push();
      for(let k = 0; k < leafLength; k++){
        var leafDiam = map(k, 0, leafLength, 0, leafDia);
-       var x = this.leafX + cos(this.leafDir - PI)*k;
-       var y = this.leafY + sin(this.leafDir - PI)*k;
-       //var x = 0;
-       //var y = k;
+       //var x = this.leafX + cos(this.leafDir - PI)*k;
+       //var y = this.leafY + sin(this.leafDir - PI)*k;
+       var x = 4;
+       var y = k + 1;
          //fill(this.hue, 50, 100, 30);
          stroke(this.hue, 50, 100, 0);
          ellipse(x, y, leafDiam, leafDiam*.7);
      }
-
+     //pop();
    }
  }
